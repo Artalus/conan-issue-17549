c527e009708345edbc21fd4ee86a6268eeff09f4
Artalus <artalus-mail@yandex.ru>
2025-02-14 18:36:31 +0400

    add metaproject / monolithic build wrap

---
+++ conanfilews.py
@@ -0,0 +1,15 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeDeps, CMakeToolchain, cmake_layout
+class MyWs(ConanFile):
+    """ This is a special conanfile, used only for workspace definition of layout
+    and generators. It shouldn't have requirements, tool_requirements. It shouldn't have
+    build() or package() methods
+    """
+    settings = "os", "compiler", "build_type", "arch"
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.generate()
+    def layout(self):
+        cmake_layout(self)
